b597f261d2d46e95d512ac97bb75f41c9bb015d7
Sho Hashimoto <hashimoto@shokai.org>
2013-06-02 06:41:55 +0900

    refact

--- linda-light-notify.rb
+++ linda-light-notify.rb
@@ -4,7 +4,7 @@ require 'rubygems'
 require 'sinatra/rocketio/linda/client'
 $stdout.sync = true
 
-url =   ENV["LINDA_BASE"] || ARGV.shift || "http://localhost:5000"
+url = ENV["LINDA_BASE"] || ARGV.shift || "http://localhost:5000"
 spaces = (ENV["LINDA_SPACES"]||ENV["LINDA_SPACE"]||"test").split(/,/)
 puts "connecting.. #{url}"
 linda = Sinatra::RocketIO::Linda::Client.new url
@@ -20,41 +20,38 @@ lasts = Hash.new{|h,k| h[k] = Hash.new }
 linda.io.on :connect do  ## RocketIO's "connect" event
   puts "connect!! <#{linda.io.session}> (#{linda.io.type})"
   tss.each do |name, ts|
-    ts.watch ["sensor", "light"] do |tuple|
+    ts.watch ["sensor", "light"]{|tuple|
       next unless tuple.size == 3 and (tuple[2].to_s =~ /^[\d\.]+$/)
       puts "#{name} - #{tuple}"
       light = tuple[2].to_i
-      if lasts[name] and lasts[name][:value]
-        light_d = light - lasts[name][:value]
-        msg = nil
-        stat = nil
-        if light_d > 20
-          msg = "#{name}で電気が点きました"
-          stat = "on"
-        elsif light_d < -20
-          msg = "#{name}で電気が消えました"
-          stat = "off"
-        end
-        if msg
-          puts msg
-          if !lasts[name][:notify] or
-              lasts[name][:notify] < (Time.now-5) or
-              (lasts[name][:stat] and lasts[name][:stat] != stat)
-            tss.values.each do |ts_|
-              ts_.write ["skype", "send", "#{msg} - #{tuple}"]
-              ts_.write ["twitter", "tweet", "#{msg} - #{tuple}"]
-            end
-            tss.each do |name_, ts_|
-              next if name_ == name
-              ts_.write ["say", msg]
-            end
+      unless lasts[name] and lasts[name][:value]
+        lasts[name][:value] = light
+        next
+      end
+      msg = nil
+      stat = nil
+      if 20 < light-lasts[name][:value]
+        msg = "#{name}で電気が点きました"
+        stat = :on
+      elsif light - lasts[name][:value] < -20
+        msg = "#{name}で電気が消えました"
+        stat = :off
+      end
+      if [:on, :off].include? stat
+        puts msg
+        if lasts[name][:stat] != stat or
+            lasts[name][:notify_at]+5 < Time.now
+          tss.each do |name_, ts_|
+            ts_.write ["skype", "send", "#{msg} - #{tuple}"]
+            ts_.write ["twitter", "tweet", "#{msg} - #{tuple}"]
+            ts_.write ["say", msg] if name_ != name
+            ts_.write ["sensor", "light", stat] if name_ == name
           end
-          lasts[name][:stat] = stat
-          lasts[name][:notify] = Time.now
         end
+        lasts[name] = {:stat => stat, :notify_at => Time.now}
       end
       lasts[name][:value] = light
-    end
+    }
   end
 end
 
